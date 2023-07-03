f6234071fc959c3f869f965d3922d3000768a2c7
sunnkey <sunweijie12345@163.com>
2023-07-03 20:49:16 +0800

    1

--- 案例/1.py
+++ 案例/1.py
@@ -1,3 +1,7 @@
+# 要求
+# 字体25px，颜色灰色，边框圆角8px
+# 信息提示级别：正常-绿边框，绿色字，警告-黄边框，黄色字，错误-红边框，红色字
+
 from PyQt5.Qt import *
 import sys
 
@@ -7,23 +11,18 @@ class Window(QWidget):
         super(Window, self).__init__()
         self.setWindowTitle('QT学习')
         self.resize(500, 500)
+        self.move(400, 400)
         self.setup_ui()
 
     def setup_ui(self):
         self.test_q_object()
 
-    @staticmethod
-    def test_q_object():
-        # mros = QObject.mro()
-        # for mro in mros:
-        #     print(mro)
-        obj = QObject()
-        obj.setObjectName('sun')
-        print(obj.objectName())
-        obj.setProperty('name', 'liu')
-        obj.setProperty('age', 20)
-        print(obj.property('age'))
-        print(obj.property('name'))
+    def test_q_object(self):
+        label1 = QLabel(self)
+        label1.setText('第一个label')
+        label2 = QLabel(self)
+        label2.setText('第二个label')
+        label2.move(100, 0)
 
 
 if __name__ == '__main__':
