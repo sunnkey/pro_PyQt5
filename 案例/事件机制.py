f813353ab0999c8842b34b33a4ca50c0a2001633
sunnkey <sunweijie12345@163.com>
2023-07-06 19:11:57 +0800

    1

--- 案例/事件机制.py
+++ 案例/事件机制.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 from PyQt5.Qt import *
 import sys
 
@@ -7,10 +7,20 @@ class APP(QApplication):
     def __init__(self, argv):
         super(APP, self).__init__(argv)
 
-    def notify(self, receiver: QtCore.QObject, event: QtCore.QEvent) -> bool:
-        if receiver.inherits('QPushButton') and event.type() == QEvent.MouseButtonPress:
-            print(receiver, event)
-        return super().notify(receiver, event)
+    def notify(self, r: QtCore.QObject, e: QtCore.QEvent) -> bool:
+        if r.inherits('QPushButton') and e.type() == QEvent.MouseButtonDblClick:
+            print(e.type())
+        return super().notify(r, e)
+
+
+class BTN(QPushButton):
+    def event(self, e: QtCore.QEvent) -> bool:
+        if e.type() == QEvent.MouseButtonPress:
+            print(e.type())
+        return super().event(e)
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
+        print('鼠标被点击了')
 
 
 app = APP(sys.argv)
@@ -18,7 +28,7 @@ window = QWidget()
 window.resize(800, 600)
 window.move(1200, 960)
 
-btn_test = QPushButton(window)
+btn_test = BTN(window)
 btn_test.setText('测试按钮')
 
 window.show()
