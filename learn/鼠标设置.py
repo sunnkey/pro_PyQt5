0cda178faaae334477e4ffbfaec99f2892c2e376
sunnkey <sunweijie12345@163.com>
2023-07-11 16:49:30 +0800

    1

--- learn/鼠标设置.py
+++ learn/鼠标设置.py
@@ -1,11 +1,28 @@
+from PyQt5 import QtGui
 from PyQt5.Qt import *
 import sys
+
+
+def my_cursor(path):
+    mouse_map = QPixmap(path)
+    mouse_map = mouse_map.scaled(50, 50)
+    my_mouse = QCursor(mouse_map)
+    return my_mouse
+
+
+class MyWindow(QWidget):
+    def mouseMoveEvent(self, e: QtGui.QMouseEvent) -> None:
+        print(e.pos())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    window = QWidget()
-    window.setCursor(Qt.ClosedHandCursor)
+    window = MyWindow()
+    cursor = my_cursor('../source/images/add.png')
+    window.setCursor(cursor)
+    window.setMouseTracking(True)
+
     window.show()
 
     sys.exit(app.exec())
