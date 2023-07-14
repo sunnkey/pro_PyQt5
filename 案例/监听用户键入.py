a7da65b91bce19dbe6f0c9e14d3ec6615285d0fc
sunnkey <sunweijie12345@163.com>
2023-07-14 17:27:14 +0800

    1

---
+++ 案例/监听用户键入.py
@@ -0,0 +1,26 @@
+from PyQt5 import QtGui
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle('监控用户输入组合键')
+        self.resize(500, 500)
+        self.move(500, 500)
+        self.set_ui()
+
+    def set_ui(self):
+        pass
+
+    def keyPressEvent(self, e: QtGui.QKeyEvent) -> None:
+        if e.key() == Qt.Key_NumLock:
+            print('ok!')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
