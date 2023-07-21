e6065da78eb4317a042e634ff19f1803ca5fb01b
sunnkey <sunweijie12345@163.com>
2023-07-21 14:55:21 +0800

    1

---
+++ Projects/Snake Game/main.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtGui
+from PyQt5.Qt import *
+import sys
+
+
+class SnakeGame(QMainWindow):
+    def __init__(self):
+        super(SnakeGame, self).__init__()
+        self.initUI()
+        # 添加游戏画布
+        self.game_board = GameBoard()
+        self.setCentralWidget(self.game_board)
+        # 初始化游戏
+        self.init_game()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle('Snake Game')
+        self.show()
+
+
+class GameBoard(QWidget):
+    def paintEvent(self, a0: QtGui.QPaintEvent) -> None:
+        qp = QPainter(self)
+        qp.fillRect(0, 0, self.width(), self.height(), Qt.white)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    game = SnakeGame()
+    sys.exit(app.exec())
