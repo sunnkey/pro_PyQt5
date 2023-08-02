10f8794b85d556cc36e14deccdccec0c3bc45d59
sunnkey <sunweijie12345@163.com>
2023-08-02 19:32:14 +0800

    1

--- 案例/qt_qAbstractButton/按钮自增加.py
+++ 案例/qt_qAbstractButton/按钮自增加.py
@@ -5,15 +5,33 @@ import sys
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        self.setWindowTitle('')
+        self.setWindowTitle('按钮增加数字')
         self.resize(500, 500)
         self.move(400, 400)
+        self.button = QPushButton(self)
+        self.count = 0
         self.setup_ui()
 
     def setup_ui(self):
-        pass
+        button_icon = QIcon('../../source/images/add.png')
+        self.button.setText(f'{self.count}')
+        self.button.setIcon(button_icon)
+        self.button.setIconSize(QSize(100, 100))
+        self.button.setShortcut('Alt+b')
+        self.button.setAutoRepeat(True)
+        self.button.setAutoRepeatInterval(500)
+        self.button.setAutoRepeatDelay(1000)
+        # self.shortcut = QShortcut(QKeySequence('Alt+b'), self)
+        # self.shortcut.activated.connect(self.add_num)
+        self.button.clicked.connect(self.add_num)
+
+    def add_num(self):
+        self.count += 1
+        self.button.setText(str(self.count))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    window = Window()
+    window.show()
     sys.exit(app.exec())
