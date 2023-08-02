4111df0117c9f04c08a48b21303abeed78f971c2
sunnkey <sunweijie12345@163.com>
2023-08-02 09:04:38 +0800

    1

---
+++ QAbstractButton/test.py
@@ -0,0 +1,22 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle('')
+        self.resize(500, 500)
+        self.move(400, 400)
+        self.setup_ui()
+
+    def setup_ui(self):
+        button = QPushButton(self)
+        button.setText('1111')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
