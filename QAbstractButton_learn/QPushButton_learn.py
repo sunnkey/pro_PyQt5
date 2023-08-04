3765a920ab53a4e2c68a2f62e54215e310a2f480
sunnkey <sunweijie12345@163.com>
2023-08-04 08:30:18 +0800

    1

---
+++ QAbstractButton_learn/QPushButton_learn.py
@@ -0,0 +1,24 @@
+from PyQt5.Qt import *
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setWindowTitle('')
+        self.resize(500, 500)
+        self.move(400, 400)
+        self.setup_ui()
+
+    def setup_ui(self):
+        button = QPushButton(self)
+        button.setText('click')
+        button.setStyleSheet('QPushButton:pressed {background-color:red}')
+        button.setCheckable(True)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
