731ee431339d3197b775fc62987cc3cf17259fbd
sunnkey <sunweijie12345@163.com>
2023-08-04 15:23:58 +0800

    1

--- QAbstractButton_learn/QPushButton_learn.py
+++ QAbstractButton_learn/QPushButton_learn.py
@@ -5,16 +5,28 @@ import sys
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
-        self.setWindowTitle('')
+        self.button = None
+        self.setWindowTitle('QPushButton')
         self.resize(500, 500)
         self.move(400, 400)
         self.setup_ui()
 
     def setup_ui(self):
-        button = QPushButton(self)
-        button.setText('click')
-        button.setStyleSheet('QPushButton:pressed {background-color:red}')
-        button.setCheckable(True)
+        self.button = QPushButton(self)
+        self.button.setText('click')
+        # button.setStyleSheet('QPushButton:pressed {background-color:red}')
+        # button.setCheckable(True)
+
+        # 创建menu对象
+        menu = QMenu()
+        # 创建action对象
+        action = QAction()
+        action.setText('新建')
+        action.setIcon(QIcon('../source/images/add.png'))
+        # 将action对象添加到menu对象中
+        menu.addAction(action)
+        # 将menu添加到button中
+        self.button.setMenu(menu)
 
 
 if __name__ == '__main__':
