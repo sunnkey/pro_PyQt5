c50836772d760efaf72d030057bab01ffc14ea40
sunnkey <sunweijie12345@163.com>
2023-08-31 15:18:27 +0800

    1

--- QComboBox_pro/1.py
+++ QComboBox_pro/1.py
@@ -14,15 +14,24 @@ class Window(QWidget):
         combo_box = QComboBox(self)
         combo_box.setGeometry(150, 150, 160, 30)
         # 增加下拉选项
-        combo_box.addItem('选项一')
+        combo_box.addItem(QIcon('../source/images/add.png'), '选项一', {'name': 'test'})
         combo_box.addItem('选项二')
         combo_box.addItem('选项三')
         combo_box.addItem('选项四')
         combo_box.insertSeparator(3)
+        combo_box.insertSeparator(5)
+
         combo_box.addItem(QIcon('../source/images/add.png'), '选项五')
         combo_box.addItems(['选项六', '选项七'])
+        # 修改指定下拉框
+        combo_box.setItemText(0, '选项1')
+        combo_box.setEditText('选项三')
         # 生成按钮
         button = QPushButton('点击', self)
+        # 获取信息
+        button.clicked.connect(lambda: print(self.findChild(QComboBox).itemData(0)))
+        # 信号
+        combo_box.highlighted
 
 
 if __name__ == '__main__':
