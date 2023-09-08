86f6db1e3a66f9b34d68277a08bca304a0a6f0f4
sunnkey <sunweijie12345@163.com>
2023-09-08 17:55:01 +0800

    1

--- QProcessBar_pro/main.py
+++ QProcessBar_pro/main.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5.Qt import *
 import sys
 
@@ -10,7 +12,19 @@ class Window(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
+        # 添加进度条和按钮
         progress_bar = QProgressBar(self)
+        progress_bar.setGeometry(50, 50, 200, 30)
+        progress_bar.setRange(0, 100)
+        progress_bar.setValue(50)
+        button = QPushButton('Start', self)
+
+        # 添加信号和槽
+        button.clicked.connect(self.slot_progress_start)
+
+    def slot_progress_start(self):
+        progress_bar = self.findChild(QProgressBar)
+        progress_bar.reset()
 
 
 if __name__ == '__main__':
